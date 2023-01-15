7556c84b29cd8b9711bb407ca2baa637fd51b151
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-01-15 23:22:25 +0530

    add contact card

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,16 +1,14 @@
+import ContactCard from "./ContactCard";
+
+
+
 const ContactList = (props) => {
 
     const renderContactList = props.contacts.map((contact) => {
 
 
     return (  
-        <div className="item">
-            <div className="content">
-                <div className="header">{contact.name}</div>
-                <div>{contact.email}</div>
-            </div>
-            <i className="trash alternate outline icon"></i>
-        </div>
+      <ContactCard contact={contact}></ContactCard>
     );
 });
 return <div className="ui celled list">{renderContactList}</div>
