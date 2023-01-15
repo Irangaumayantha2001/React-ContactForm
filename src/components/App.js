aa33985353fc1a76a25888d3608c7c148db0d4a0
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-01-15 22:23:14 +0530

    frist comment

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Header from './Header'
+import AddContact from './AddContact'
+import ContactList from './ContactList'
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <AddContact/>
+        <ContactList/>
+    </div>
+  );
+}
+
+export default App;
